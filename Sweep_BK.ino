fdf8f6c04a7abf25b80c0d548ed79193586603ed
Ben Kadosh <52785975+BenKadosh1@users.noreply.github.com>
2019-10-10 02:15:42 -0400

    Add files via upload

---
+++ Sweep_BK.ino
@@ -0,0 +1,26 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int mapped_value = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  mapped_value = map(sensorValue, 0, 1023, 0, 180);
+  myservo.write(mapped_value);
+  delay(50);
+}
